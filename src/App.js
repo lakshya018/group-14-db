569bcc04c00ec0ecc18b36821d52a644531cea55
Lakshya Soni <69206138+lakshya018@users.noreply.github.com>
2023-08-07 12:16:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 // import Sidebar from './Components/Sidebar/Sidebar';
 import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter } from 'react-router-dom';
-import { useState } from 'react';
+// import { useState } from 'react';
 // import Dashboard from './Components/Dashboard/Dashboard';
 import Trade from './Components/Trade/Trade';
 

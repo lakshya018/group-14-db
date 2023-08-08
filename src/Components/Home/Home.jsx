77e710589d2fad46df0ca5a910793a9020f3acda
Lakshya Soni <69206138+lakshya018@users.noreply.github.com>
2023-08-08 17:33:49 +0530

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Select, MenuItem, Button } from "@mui/material";
 import "./Home.css"
 import { useAuth0 } from '@auth0/auth0-react';
+import BondCards from '../BondCards/BondCards';
 
 const Home = () => {
 
@@ -24,44 +25,50 @@ const Home = () => {
     }
 
     return (
-        <div className='home-container'>
+        <>
+
             {
-                isAuthenticated?
-                <>
-                <h3 className='heading-name'>Hi {user.name}, welcome to the Stonks App</h3>
-            <div className="search-container">
-                <Select
-                    sx={{
-                        marginRight: 1,    
-                    }}
-                    defaultValue={1}
-                    onChange={handleSelect}
-                    className='select-option'
-                    variant="standard"
-                    disableUnderline
-                >
-                    <MenuItem value={1}>ISIN</MenuItem>
-                    <MenuItem value={2}>Company</MenuItem>
+                isAuthenticated ?
+                    <>
+                        <div className='home-container'>
+                            <h3 className='heading-name'>Hi {user.name}, welcome to the Stonks App</h3>
+                            <div className="search-container">
+                                <Select
+                                    sx={{
+                                        marginRight: 1,
+                                    }}
+                                    defaultValue={1}
+                                    onChange={handleSelect}
+                                    className='select-option'
+                                    variant="standard"
+                                    disableUnderline
+                                >
+                                    <MenuItem value={1}>ISIN</MenuItem>
+                                    <MenuItem value={2}>Company</MenuItem>
 
-                </Select>
-
-
-                <input type="search" placeholder={placeHolder} className='search-bar' onChange={handleChange} />
-            </div>
+                                </Select>
 
 
-            <div className="search-btn">
-                <Button variant='contained'>Search</Button>
-            </div>
-                </>
-                :
-                <h3 className='heading-name'>Hi, welcome to the Stonks app. Login to see the status of the bonds</h3>
+                                <input type="search" placeholder={placeHolder} className='search-bar' onChange={handleChange} onKeyDown={handleSearch} />
+                            </div>
+
+
+                            <div className="search-btn">
+                                <Button variant='contained'>Search</Button>
+                            </div>
+                        </div>
+                        <BondCards />
+                    </>
+                    :
+                    <div className="home-container">
+                        <h3 className='heading-name'>Hi, welcome to the Stonks app. Login to see the status of the bonds</h3>
+                    </div>
+
+
+
             }
-
-            
-
-
-        </div>
+        
+        </>
     )
 }
 

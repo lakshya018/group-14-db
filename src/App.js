99d4e22a9eb5a680db7494ce8a340156461fe995
Lakshya Soni <69206138+lakshya018@users.noreply.github.com>
2023-08-08 17:32:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-import { BrowserRouter} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home/Home';
-import BondCards from './Components/BondCards/BondCards';
 import { useAuth0 } from '@auth0/auth0-react';
-
+import AppState from './context/appState';
+import Securities from './Components/Securities/Securities';
 function App() {
   const { isAuthenticated } = useAuth0();
   return (
     <BrowserRouter>
-    <div className="App">
-      <Navbar/>
-      <Home/>
-      {
-        isAuthenticated ?
-        <BondCards/>
-        :
-        <></>
-      }
-      
-    </div>
+      <AppState>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path="/securities" element={<Securities />} />
+          </Routes>
+
+        </div>
+      </AppState>
+
     </BrowserRouter>
-    
+
   );
 }
 

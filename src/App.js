eda59d30ee2dc72cc829b741c663f31c363258d8
Lakshya Soni <69206138+lakshya018@users.noreply.github.com>
2023-08-08 18:01:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navbar from './Components/Navbar/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import Securities from './Components/Securities/Securities';
+import Trade from './Components/Trade/Trade';
+
 function App() {
   
   return (
@@ -13,6 +15,7 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path="/securities" element={<Securities />} />
+            <Route path="/trades" element={<Trade />} />
           </Routes>
 
         </div>

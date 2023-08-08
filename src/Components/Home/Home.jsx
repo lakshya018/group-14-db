b85934ee6f6accc841b5a848fec5dbeaf92b1df2
Lakshya Soni <69206138+lakshya018@users.noreply.github.com>
2023-08-08 18:46:57 +0530

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,18 +1,30 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Select, MenuItem, Button } from "@mui/material";
 import "./Home.css"
 import { useAuth0 } from '@auth0/auth0-react';
+import appContext from '../../context/appContext';
 import BondCards from '../BondCards/BondCards';
+import FilterSearch from '../FilterSearch/FilterSearch';
 
 const Home = () => {
 
     const [placeHolder, setPlaceHolder] = useState("ISIN");
     const [searchText, setSearchText] = useState(""); // eslint-disable-line
     const { isAuthenticated, user } = useAuth0();
+    const [filterSearch, setFilterSearch] = useState([]);
+    
+
+    const context = useContext(appContext);
+    const { securities, getSecurities } = context;;
 
     const handleChange = (e) => {
         setSearchText(e.target.value);
     }
+
+    useEffect(()=>{
+        getSecurities();
+        // eslint-disable-next-line
+    },[])
 
     const handleSelect = (e) => {
         // e.preventDefault();
@@ -24,6 +36,37 @@ const Home = () => {
         }
     }
 
+    const handleSearch = (e) => {
+        
+        if (e.key === "Enter" || e.type === "click") {
+
+            // console.log(securities)
+            const arr = [];
+            if(searchText.length === 0){
+                alert("Please enter something to search!");
+                return;
+            }
+           
+            else if (placeHolder === "ISIN") {
+                for (let i = 0; i < securities.length; i++) {
+                    if (securities[i].ISIN === searchText) {
+                        arr.push(securities[i]);
+                    }
+                }
+                setFilterSearch(arr);
+                
+            }
+            else {
+                for (let i = 0; i < securities.length; i++) {
+                    if (securities[i].Issuer === searchText) {
+                        arr.push(securities[i]);
+                    }
+                }
+                setFilterSearch(arr);
+            }
+        }
+        else return;
+    }
     return (
         <>
 
@@ -49,14 +92,15 @@ const Home = () => {
                                 </Select>
 
 
-                                <input type="search" placeholder={placeHolder} className='search-bar' onChange={handleChange} />
+                                <input type="search" placeholder={placeHolder} className='search-bar' onChange={handleChange} onKeyDown={handleSearch} />
                             </div>
 
 
                             <div className="search-btn">
-                                <Button variant='contained'>Search</Button>
+                                <Button variant='contained' onClick={handleSearch}>Search</Button>
                             </div>
                         </div>
+                        <FilterSearch data={filterSearch}/>
                         <BondCards />
                     </>
                     :
@@ -72,4 +116,4 @@ const Home = () => {
     )
 }
 
-export default Home
+export default Home;

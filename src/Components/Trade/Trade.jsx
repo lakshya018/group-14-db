e13c4879066e1cb6ffcc7799a85fd7a014c21ac8
Darshan-P2331 <darshanp2331@gmail.com>
2023-08-08 17:30:24 +0530

    Trades page

--- src/Components/Trade/Trade.jsx
+++ src/Components/Trade/Trade.jsx
@@ -1,10 +1,23 @@
 import { DataGrid, GridToolbar } from '@mui/x-data-grid';
+import { Modal, Box, Typography, Divider } from "@mui/material";
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import './Trade.css'
 
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    p: 4,
+};
+
 const Trade = () => {
     const [tradesData, setTradesData] = useState([])
+    const [securityData, setSecurityData] = useState(false)
 
     useEffect(() => {
         const fetchData = async () => {
@@ -21,6 +34,12 @@ const Trade = () => {
         }
         fetchData()
     }, [])
+
+    const handleSecurityIdClick = async (securityId) => {
+        const res = await axios.get(`https://mybond-production.up.railway.app/api/securities/${securityId}`)
+        console.log(res.data)
+        setSecurityData(res.data)
+    }
 
     const columns = [
         {
@@ -68,12 +87,43 @@ const Trade = () => {
             </h2>
             <div className='trade__body'>
                 <div className="trade__container">
+                <Modal
+                        open={securityData !== false}
+                        onClose={() => setSecurityData(false)}
+                        aria-labelledby="modal-modal-title"
+                        aria-describedby="modal-modal-description"
+                    >
+                        <Box sx={style}>
+                            <Typography id="modal-modal-title" variant="h6" component="h2" fontWeight='bold'>
+                                Security Details
+                            </Typography>
+                            <Divider />
+                            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                                ISIN: {securityData.ISIN}
+                                <br />
+                                CUSIP: {securityData.CUSIP}
+                                <br />
+                                Issuer: {securityData.Issuer}
+                                <br />
+                                MaturityDate: {new Date(securityData.MaturityDate).toLocaleDateString()}
+                                <br />
+                                Coupon: {securityData.Coupon}
+                                <br />
+                                Type: {securityData.Type}
+                                <br />
+                                FaceValue: Rs {securityData.FaceValue}
+                                <br />
+                                Status: {securityData.Status}
+                                <br />
+                            </Typography>
+                        </Box>
+                    </Modal>
                     <DataGrid
                         columns={columns}
                         rows={tradesData}
                         slots={{ toolbar: GridToolbar }}
                         initialState={{
-                            pagination: { paginationModel: { pageSize: 25 } }
+                            pagination: { paginationModel: { pageSize: 10 } }
                         }}
                         componentsProps={{
                             toolbar: {
@@ -81,6 +131,7 @@ const Trade = () => {
                                 printOptions: { disableToolbarButton: true }
                             }
                         }}
+                        onCellClick={(cell) => handleSecurityIdClick(cell.value)}
                     />
                 </div>
             </div>
